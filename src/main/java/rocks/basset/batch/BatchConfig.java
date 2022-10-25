c99510954af18a0fc97a3ffb59ae0156efb9d214
BASSET, Thomas <thomas.basset@cgi.com>
2022-10-25 14:23:52 +0200

    Initialisation du job avec définition d'un validator.

---
+++ src/main/java/rocks/basset/batch/BatchConfig.java
@@ -0,0 +1,42 @@
+package rocks.basset.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+    @Bean
+    public JobParametersValidator defaultJobParametersValidator(){
+        DefaultJobParametersValidator bean = new DefaultJobParametersValidator();
+        bean.setRequiredKeys(new String[] {"formateursFile","formationsFile","seancesFile"});
+        return bean;
+    }
+
+    @Bean
+    public Step step1(final StepBuilderFactory stepBuilderFactory){
+        return stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
+                .build();
+    }
+
+    @Bean
+    public Job job(final JobBuilderFactory jobBuilderFactory){
+        return jobBuilderFactory.get("formation-batch")
+                .start(step1(null))
+                .validator(defaultJobParametersValidator())
+                .build();
+    }
+}
