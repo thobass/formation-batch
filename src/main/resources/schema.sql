9603acf37408f5b194d190bd499530584aed9ee0
BASSET, Thomas <thomas.basset@cgi.com>
2022-10-25 14:22:58 +0200

    Init commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,46 @@
+--
+-- Base de données :  formations-db
+--
+
+-- --------------------------------------------------------
+DROP TABLE IF EXISTS seances;
+DROP TABLE IF EXISTS formations;
+DROP TABLE IF EXISTS formateurs;
+
+
+
+CREATE TABLE formateurs (
+                            id int NOT NULL,
+                            nom varchar(12) NOT NULL,
+                            prenom varchar(12) NOT NULL,
+                            adresse_email varchar(50) NOT NULL
+);
+
+CREATE TABLE formations (
+                            code varchar(20) NOT NULL,
+                            libelle varchar(200) NOT NULL,
+                            descriptif varchar(500) NOT NULL
+);
+
+CREATE TABLE seances (
+                         code_formation varchar(20) NOT NULL,
+                         id_formateur int NOT NULL,
+                         date_debut date NOT NULL,
+                         date_fin date NOT NULL
+);
+
+
+ALTER TABLE formateurs
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE formations
+    ADD PRIMARY KEY (code);
+
+ALTER TABLE seances
+    ADD PRIMARY KEY (code_formation,id_formateur);
+
+
+ALTER TABLE seances
+    ADD CONSTRAINT FK_FORMATEURS FOREIGN KEY (id_formateur) REFERENCES formateurs (id);
+ALTER TABLE seances
+    ADD CONSTRAINT FK_FORMATIONS FOREIGN KEY (code_formation) REFERENCES formations (code);
---
+++ src/test/java/rocks/basset/batch/FormationsBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.basset.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationsBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
